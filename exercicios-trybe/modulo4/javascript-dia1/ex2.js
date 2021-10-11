5314bbf75cdb5c8c20709d3427d0e575b1f3da5c
EuSouLuis <luisaraujodemelo@hotmail.com>
2021-10-11 15:48:28 -0300

    exercicio2

---
+++ exercicios-trybe/modulo4/javascript-dia1/ex2.js
@@ -0,0 +1,12 @@
+const a = Math.floor(Math.random() * 100);
+const b =  Math.floor(Math.random() *100);
+const p = "foram gerados a e b respectivamente. A é maior dos 2";
+const q = "foram gerados a e b respectivamente. B é maior";
+if(a > b){
+    console.log(a); console.log(b);
+    console.log(p);
+}
+else{
+    console.log(a); console.log(b);
+    console.log( q);
+}
