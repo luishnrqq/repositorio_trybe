4fa7232efffbdf9d423610cf38009c7c6c5c4c90
EuSouLuis <luisaraujodemelo@hotmail.com>
2021-10-11 15:08:46 -0300

    exercicio1 javascript

---
+++ exercicios-trybe/modulo4/javascript-dia1/ex1.js
@@ -0,0 +1,14 @@
+const a = 35;
+const b = 12;
+
+let adição = a+b;
+let subtraçao = a-b;
+let multiplicaçao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(adição);
+console.log(subtraçao);
+console.log(multiplicaçao);
+console.log(divisao);
+console.log(modulo);
