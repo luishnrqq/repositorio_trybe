352062f9c3a4493534206cdaf9919ac1b2698016
EuSouLuis <luisaraujodemelo@hotmail.com>
2021-10-11 17:01:46 -0300

    exercicio5

---
+++ exercicios-trybe/modulo4/javascript-dia1/ex5.js
@@ -0,0 +1,10 @@
+const lado1 = 80;
+const lado2 = 90;
+const lado3 = 40;
+
+if(lado1+lado2+lado3 == 180){
+console.log("triangulo válido");
+}
+else{
+    console.log("Erro! não é um triangulo válido");
+}
